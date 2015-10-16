9110c9d31d40ac9e9b6714581bbfcb8ed587e154
Chris Hill <hello@christopherjameshill.com>
2015-10-16 16:14:14 +0200

    Updated HomePage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Christopher</div>
+                <div class="title">Christopher James Hill</div>
             </div>
         </div>
     </body>
