c80d594d611d1dd18ff3c0f172424489e61e7e63
Chris Hill <hello@christopherjameshill.com>
2015-10-22 19:22:29 +0200

    Updated Elixir to merge stylesheets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
         @yield('container')
         
     <script src="https://use.typekit.net/ihu8pda.js"></script>
-    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>    
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>     
     <script src="/js/main.js" type="text/javascript"></script>
 </body>
