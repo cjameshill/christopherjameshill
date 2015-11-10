725b5f61f09520b6961b655b75ecc8d089daf89e
Chris Hill <hello@christopherjameshill.com>
2015-11-10 18:19:59 +0100

    Initial Commit

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,19 +1,11 @@
 @extends('app')
  @section('container')
-      @include('partials.offcanvas')
-    
-    <div class="main">   
-       
-        @include('partials.topbar')
+   
       
         <div id="contentWrapper">  
         @yield('content')
        
        </div>
-        @include('partials.footer')
-       
-       
-    </div> <!-- Main -->
-       
+      
     @endsection
     
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,17 +2,10 @@
 
 @section('content')
     
-    <section class="first" id="section_cover">
-           <div class="content">
-              <header class="sectionHeader">
-                  <h1>Welcome.</h1>
-              </header>
-               
-            <div class="grid">
-                    <div class="item">
-                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita perspiciatis pariatur sit ratione minus, a impedit esse distinctio nemo repellat excepturi officia illum quas velit natus praesentium accusamus, unde. Numquam.</p>
-                        </div>
-                </div>               </div>
+    <section id="section_cover">
+           <div class="flex-container">
+               <div class="new">Christopher James Hill</div>
+           </div>
         </section>
       
 
